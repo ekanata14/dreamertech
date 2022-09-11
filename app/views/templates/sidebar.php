11918d7fc85c05390ef97ef27c550aecab6c6dba
ekanata14 <ekanata1411@gmail.com>
2022-09-11 21:30:27 +0800

    Adding ram-img.png

--- app/views/templates/sidebar.php
+++ app/views/templates/sidebar.php
@@ -4,7 +4,7 @@
         <li class="nav-item mb-3"><a href="<?= BASE_URL ?>/products" class="text-white text-decoration-none"><img class="w-50" src="<?= BASE_URL ?>/images/cpu-img.png" alt="cpu-img">
         </a></li>
         <li class="nav-item mb-3"><a href="<?= BASE_URL ?>/products/gpu" class="text-white text-decoration-none"><img class="w-50" src="<?= BASE_URL ?>/images/gpu-img.png" alt="gpu-img"></a></li>
-        <li class="nav-item mb-3"><a href="<?= BASE_URL ?>/products/ram" class="text-white text-decoration-none">RAM (Random Access Memory)</a></li>
+        <li class="nav-item mb-3"><a href="<?= BASE_URL ?>/products/ram" class="text-white text-decoration-none"><img class="w-50" src="<?= BASE_URL ?>/images/ram-img.png" alt="ram-img"></a></li>
         <li class="nav-item mb-3"><a href="<?= BASE_URL ?>/products/storage" class="text-white text-decoration-none"><img class="w-50" src="<?= BASE_URL ?>/images/storage-img.png" alt="storage-img"></a></li>
         <li class="nav-item mb-3"><a href="<?= BASE_URL ?>/products/fan" class="text-white text-decoration-none"><img class="w-50" src="<?= BASE_URL ?>/images/fan-img.png" alt="fan-img"></li>
         <li class="nav-item mb-3"><a href="<?= BASE_URL ?>/products/case" class="text-white text-decoration-none">Case</a></li>
